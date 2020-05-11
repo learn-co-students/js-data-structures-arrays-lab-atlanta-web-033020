9369a0deadd32f257b30a2efb5e9ed5c941b1363
rpdecks <rpdecks@gmail.com>
2020-05-10 21:10:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"]
+let new_ary = [];
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name){
+    drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(){
+    drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver(){
+    drivers.shift(name);
+}
+
+function appendDriver(name){
+    return new_ary = [...drivers, name];
+}
+
+function prependDriver(name){
+    return new_ary = [name, ...drivers];
+}
+
+function removeLastDriver(){
+    return new_ary = drivers.slice(0, drivers.length - 1)
+}
+
+function removeFirstDriver(){
+    return new_ary = drivers.slice(1);
+}
